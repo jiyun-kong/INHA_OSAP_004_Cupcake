0fd3f7889c28d44b172df6c69258b1d372e7cf6e
jiyun-kong <kjyn2001@gmail.com>
2023-11-19 21:14:03 +0900

    add main.cpp, GoogleTest

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,52 @@
 /*
 * Copyright 2023 CUPCAKE Team, Inha Univ 
 * released under GPL 3.0 License
-* Contributor of this file : SungraeKim (23/11/11)
+* Contributor of this file : SungraeKim (23/11/11), JiyunKong (23/11/19)
 */
 #include <iostream>
-#include "avl_tree.hpp"
+#include "include/avl_tree.hpp"
 
 using namespace std;
 
 int main() {
-    
+  AvlTree tree;
+
+  // test if the tree is empty
+  if (tree.empty()) {
+    cout << "The tree is empty.\n";
+  } else {
+    cout << "The tree is not empty.\n";
+  }
+
+  // insert data
+  tree.insert(10);
+  tree.insert(20);
+  tree.insert(30);
+  tree.insert(40);
+  tree.insert(50);
+
+  // find value
+  if (tree.find(30)) {
+    cout << "30 found in the tree\n";
+  } else {
+    cout << "30 not found in the tree\n";
+  }
+
+  // get size
+  cout << "Size of the tree : " << tree.get_size() << "\n";
+
+  // test if the tree is empty
+  if (tree.empty()) {
+    cout << "The tree is empty.\n";
+  } else {
+    cout << "The tree is not empty.\n";
+  }
+
+  // get minimum
+  tree.minimum(20);
+
+  // get maximum
+  tree.maximum(20);
+
+  return 0;
 }
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright 2023 CUPCAKE Team, Inha Univ
  * released under GPL 3.0 License
- * Contributor of this file : SungraeKim (23/11/11)
+ * Contributor of this file : SungraeKim (23/11/11), JiyunKong (23/11/13), JiyunKong (23/11/19)
  */
 #include "avl_tree.hpp"
 #include <gtest/gtest.h>
@@ -10,7 +10,7 @@
 class AvlTreeTest : public ::testing::Test
 {
 protected:
-  AvlTree test_tree;            // AVL tree instance to use for testing
+  AvlTree test_tree;       // AVL tree instance to use for testing
   void SetUp() override {} // Initialize the tree for each test
 };
 
@@ -24,12 +24,14 @@ TEST_F(AvlTreeTest, InsertIncreaseSize)
   EXPECT_EQ(test_tree.get_size(), 2); // Test the size after another insert
 }
 
+// Test if the tree is empty
 TEST(TestEmpty, EmptyWhenInitialized)
 {
   AvlTree test_tree;
   EXPECT_TRUE(test_tree.empty()); // Tree must be empty immediately after initialization
 }
 
+// Test if the tree is empty after insertion
 TEST(TestInsertEmpty, NotEmptyAfterInsertion)
 {
   AvlTree test_tree;
@@ -37,7 +39,7 @@ TEST(TestInsertEmpty, NotEmptyAfterInsertion)
   EXPECT_FALSE(test_tree.empty()); // Tree must not be empty after the element has been inserted
 }
 
-// Test the size of the empty AvlTree
+// Test the size of the empty tree
 TEST(TestSize, EmptyAvlTreeTest)
 {
   AvlTree test_tree;
@@ -45,16 +47,16 @@ TEST(TestSize, EmptyAvlTreeTest)
   EXPECT_EQ(size, 0);
 }
 
-/*
-// Test tree structure : Test later due to the non-implementation of find()
-TEST_F(AvlTreeTest, InsertMaintainStructure) {
-    test_tree.insert(10);
-    test_tree.insert(20);
-    test_tree.insert(5);
-    test_tree.insert(15);
-    EXPECT_EQ(test_tree.find(10)->get_key(), 10); // Test a specific key in the Avl Tree
+// Test the tree structure
+TEST_F(AvlTreeTest, InsertMaintainStructure)
+{
+  AvlTree test_tree;
+  test_tree.insert(10);
+  test_tree.insert(20);
+  test_tree.insert(5);
+  test_tree.insert(15);
+  EXPECT_EQ(test_tree.find(10), 0); // Test a specific key in the Avl Tree
 }
-*/
 
 int main(int argc, char **argv)
 {
